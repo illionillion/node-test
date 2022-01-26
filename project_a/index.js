aa9bb35e432d10b5e185870ea18705ffd231b1db
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-01-26 23:51:36 +0900

    colorsのパッケージでコンソールに色付けれる。package.jsonいじればnpm run * で実行できるコマンドも作れる

---
+++ project_a/index.js
@@ -0,0 +1,10 @@
+const colors=require('colors');
+const s1='Hello Node.js';
+const s2='Hello JavaScript';
+const s3='Hello React';
+const s4='Hello hero';
+
+console.log(s1.underline.red);
+console.log(s2.inverse.blue);
+console.log(s3.rainbow);
+console.log(s4.inverse.red);
